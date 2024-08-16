738c9fee52e2afc89234236a37b6bf9e7e82ea70
VedagyaNamdev <167688489+VedagyaNamdev@users.noreply.github.com>
2024-08-16 17:09:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById("checkbox").addEventListener("click", function() {
+    const nav2 = document.querySelector(".nav-2");
+    nav2.classList.toggle("show-nav");
+  });
+
+
+
+  document.querySelector(".nav-2").classList;
+
+
+
+
+
+  
+  // window.onscroll = function() {stickyNavbar()};
+
+  // const navbar = document.getElementsByClassName("nav");
+  // const sticky = navbar.offsetTop;
+  
+  // function stickyNavbar() {
+  //     if (window.pageYoffset >= sticky) {
+  //         navbar.classList.add("sticky");
+  //     } else {
+  //         navbar.classList.remove("sticky");
+  //     }
+  // }
+  
